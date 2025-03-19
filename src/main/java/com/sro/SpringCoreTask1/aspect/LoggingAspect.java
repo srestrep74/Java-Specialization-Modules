19508329b8a219a3a0c8faa43507cc74412215a8
sebastian <srestrep74@eafit.edu.co>
2025-03-19 16:35:35 -0500

    refactor: logging

--- src/main/java/com/sro/SpringCoreTask1/aspect/LoggingAspect.java
+++ src/main/java/com/sro/SpringCoreTask1/aspect/LoggingAspect.java
@@ -17,49 +17,46 @@ public class LoggingAspect {
 
     @Before("execution(* com.sro.SpringCoreTask1.service.*.*(..))")
     public void logBeforeServiceMethod(JoinPoint joinPoint) {
-        logger.info("Executing service method: {}", joinPoint.getSignature().getName());
-        logger.debug("Method arguments: {}", joinPoint.getArgs());
+        logger.info("Starting execution of service method: {}", joinPoint.getSignature().getName());
+        logger.debug("Arguments for method {}: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
     @AfterReturning(pointcut = "execution(* com.sro.SpringCoreTask1.service.*.*(..))", returning = "result")
     public void logAfterServiceMethod(JoinPoint joinPoint, Object result) {
-        logger.info("Service method {} executed successfully. Result: {}", joinPoint.getSignature().getName(), result);
+        logger.info("Service method {} executed successfully.", joinPoint.getSignature().getName());
+        logger.debug("Result of method {}: {}", joinPoint.getSignature().getName(), result);
     }
 
     @AfterThrowing(pointcut = "execution(* com.sro.SpringCoreTask1.service.*.*(..))", throwing = "exception")
     public void logAfterServiceException(JoinPoint joinPoint, Exception exception) {
-        logger.error("Service method {} failed with exception: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
-    }
-
-    @Before("execution(* com.sro.SpringCoreTask1.repository.*.*(..))")
-    public void logBeforeRepositoryMethod(JoinPoint joinPoint) {
-        logger.info("Executing repository method: {}", joinPoint.getSignature().getName());
-        logger.debug("Method arguments: {}", joinPoint.getArgs());
-    }
-
-    @AfterReturning(pointcut = "execution(* com.sro.SpringCoreTask1.repository.*.*(..))", returning = "result")
-    public void logAfterRepositoryMethod(JoinPoint joinPoint, Object result) {
-        logger.info("Repository method {} executed successfully. Result: {}", joinPoint.getSignature().getName(), result);
-    }
-
-    @AfterThrowing(pointcut = "execution(* com.sro.SpringCoreTask1.repository.*.*(..))", throwing = "exception")
-    public void logAfterRepositoryException(JoinPoint joinPoint, Exception exception) {
-        logger.error("Repository method {} failed with exception: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
+        logger.error("Error in service method {}: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
     }
 
     @Before("execution(* com.sro.SpringCoreTask1.facade.*.*(..))")
     public void logBeforeFacadeMethod(JoinPoint joinPoint) {
-        logger.info("Executing facade method: {}", joinPoint.getSignature().getName());
-        logger.debug("Method arguments: {}", joinPoint.getArgs());
+        logger.info("Starting execution of facade method: {}", joinPoint.getSignature().getName());
+        logger.debug("Arguments for method {}: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
     @AfterReturning(pointcut = "execution(* com.sro.SpringCoreTask1.facade.*.*(..))", returning = "result")
     public void logAfterFacadeMethod(JoinPoint joinPoint, Object result) {
-        logger.info("Facade method {} executed successfully. Result: {}", joinPoint.getSignature().getName(), result);
+        logger.info("Facade method {} executed successfully.", joinPoint.getSignature().getName());
+        logger.debug("Result of method {}: {}", joinPoint.getSignature().getName(), result);
     }
 
     @AfterThrowing(pointcut = "execution(* com.sro.SpringCoreTask1.facade.*.*(..))", throwing = "exception")
     public void logAfterFacadeException(JoinPoint joinPoint, Exception exception) {
-        logger.error("Facade method {} failed with exception: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
+        logger.error("Error in facade method {}: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
+    }
+
+    @Before("execution(* com.sro.SpringCoreTask1.repository.*.*(..))")
+    public void logBeforeRepositoryMethod(JoinPoint joinPoint) {
+        logger.info("Starting execution of repository method: {}", joinPoint.getSignature().getName());
+        logger.debug("Arguments for method {}: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.sro.SpringCoreTask1.repository.*.*(..))", throwing = "exception")
+    public void logAfterRepositoryException(JoinPoint joinPoint, Exception exception) {
+        logger.error("Error in repository method {}: {}", joinPoint.getSignature().getName(), exception.getMessage(), exception);
     }
 }
