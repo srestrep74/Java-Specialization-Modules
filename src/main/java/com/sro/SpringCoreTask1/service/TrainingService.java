6eb84bbf6ccaebb04fe841bd9b019c5d85dbca20
sebastian <srestrep74@eafit.edu.co>
2025-04-09 17:36:38 -0500

    refactor: services

--- src/main/java/com/sro/SpringCoreTask1/service/TrainingService.java
+++ src/main/java/com/sro/SpringCoreTask1/service/TrainingService.java
@@ -1,8 +1,5 @@
 package com.sro.SpringCoreTask1.service;
 
-
-import java.util.List;
-
 import com.sro.SpringCoreTask1.dto.request.TrainingRequestDTO;
 import com.sro.SpringCoreTask1.dto.response.TrainingResponseDTO;
 import com.sro.SpringCoreTask1.dtos.v1.request.training.CreateTrainingRequest;
@@ -10,9 +7,16 @@ import com.sro.SpringCoreTask1.dtos.v1.request.training.TraineeTrainingFilter;
 import com.sro.SpringCoreTask1.dtos.v1.request.training.TraineeTrainingResponse;
 import com.sro.SpringCoreTask1.dtos.v1.request.training.TrainerTrainingFilter;
 import com.sro.SpringCoreTask1.dtos.v1.request.training.TrainerTrainingResponse;
-import com.sro.SpringCoreTask1.service.base.BaseService;
 
-public interface TrainingService extends BaseService<TrainingRequestDTO, TrainingResponseDTO, Long>{
+import java.util.List;
+
+public interface TrainingService {
+    TrainingResponseDTO save(TrainingRequestDTO trainingRequestDTO);
+    TrainingResponseDTO findById(Long id);
+    List<TrainingResponseDTO> findAll();
+    void deleteById(Long id);
+    TrainingResponseDTO update(TrainingRequestDTO trainingRequestDTO);
+    
     List<TraineeTrainingResponse> findTrainingsByTraineeWithFilters(TraineeTrainingFilter filterDTO);
     List<TrainerTrainingResponse> findTrainingsByTrainerWithFilters(TrainerTrainingFilter filterDTO);
     void addTraining(CreateTrainingRequest createTrainingRequest);
