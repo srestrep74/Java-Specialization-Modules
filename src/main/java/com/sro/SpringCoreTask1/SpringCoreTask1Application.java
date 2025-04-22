bad92c4048ca2c2e6523ae3c5c51dd7c22c15cfb
sebastian <srestrep74@eafit.edu.co>
2025-04-22 18:06:38 -0500

    feat: put trainee repository with jpaRepository and configure db with spring Boot

--- src/main/java/com/sro/SpringCoreTask1/SpringCoreTask1Application.java
+++ src/main/java/com/sro/SpringCoreTask1/SpringCoreTask1Application.java
@@ -2,15 +2,8 @@ package com.sro.SpringCoreTask1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    HibernateJpaAutoConfiguration.class,
-    DataSourceAutoConfiguration.class,
-    DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication
 public class SpringCoreTask1Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringCoreTask1Application.class, args);
--- src/main/java/com/sro/SpringCoreTask1/repository/TraineeRepository.java
+++ src/main/java/com/sro/SpringCoreTask1/repository/TraineeRepository.java
@@ -3,13 +3,33 @@ package com.sro.SpringCoreTask1.repository;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.sro.SpringCoreTask1.entity.Trainee;
 import com.sro.SpringCoreTask1.entity.Trainer;
-import com.sro.SpringCoreTask1.repository.base.BaseRepository;
 
-public interface TraineeRepository extends BaseRepository<Trainee, Long> {
+@Repository
+public interface TraineeRepository extends JpaRepository<Trainee, Long> {
+
     Optional<Trainee> findByUsername(String username);
-    boolean deleteByUsername(String username);
-    boolean updatePassword(Long traineeId, String newPassword);
-    Set<Trainer> findTrainersByTraineeId(Long traineeId);
+
+    @Query("SELECT t FROM Trainee t LEFT JOIN FETCH t.trainers WHERE t.username = :username")
+    Optional<Trainee> findByUsernameWithTrainers(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+
+    @Modifying
+    @Query("UPDATE Trainee t SET t.password = :newPassword WHERE t.id = :id")
+    int updatePassword(@Param("id") Long id, @Param("newPassword") String newPassword);
+
+    @Modifying
+    @Query("DELETE FROM Trainee t WHERE t.username = :username")
+    int deleteByUsername(@Param("username") String username);
+
+    @Query("SELECT tr FROM Trainee t JOIN t.trainers tr WHERE t.id = :traineeId")
+    Set<Trainer> findTrainersByTraineeId(@Param("traineeId") Long traineeId);
 }
