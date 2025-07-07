a3c8a626af88c9a01b1a7c86cd37f728831fafc8
sebastian <srestrep74@eafit.edu.co>
2025-07-07 18:37:10 -0500

    feat: creation of structure of workload microservice

---
+++ business-domain/workload-service/src/main/java/dev/sro/workload_service/WorkloadServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.sro.workload_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadServiceApplication.class, args);
+	}
+
+}
---
+++ business-domain/workload-service/src/test/java/dev/sro/workload_service/WorkloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sro.workload_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
