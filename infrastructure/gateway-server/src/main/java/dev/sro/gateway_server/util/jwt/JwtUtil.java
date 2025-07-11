e3b827c954faf6575b11032b95cefb23f4315514
sebastian <srestrep74@eafit.edu.co>
2025-07-10 22:09:13 -0500

    fix: secret key for jwt

--- infrastructure/gateway-server/src/main/java/dev/sro/gateway_server/util/jwt/JwtUtil.java
+++ infrastructure/gateway-server/src/main/java/dev/sro/gateway_server/util/jwt/JwtUtil.java
@@ -37,9 +37,8 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
+        return Jwts.parser()
                 .setSigningKey(signingKey)
-                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
