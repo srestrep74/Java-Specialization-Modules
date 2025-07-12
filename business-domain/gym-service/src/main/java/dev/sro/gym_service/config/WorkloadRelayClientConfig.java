9880bc31601a0c473b1a8cb768ffffae72e5994b
sebastian <srestrep74@eafit.edu.co>
2025-07-12 11:26:59 -0500

    fix: gym yml

--- business-domain/gym-service/src/main/java/dev/sro/gym_service/config/WorkloadRelayClientConfig.java
+++ business-domain/gym-service/src/main/java/dev/sro/gym_service/config/WorkloadRelayClientConfig.java
@@ -1,15 +1,15 @@
 package dev.sro.gym_service.config;
 
 import dev.sro.gym_service.client.ServiceTokenInterceptor;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 
-@Configuration
+
 public class WorkloadRelayClientConfig {
 
     @Bean
-    public ServiceTokenInterceptor serviceTokenInterceptor(@Value("${internal.service.token}") String serviceToken) {
+    public RequestInterceptor serviceTokenInterceptor(@Value("${internal.service.token}") String serviceToken) {
         return new ServiceTokenInterceptor(serviceToken);
     }
 } 
